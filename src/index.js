e2f629161dc30213a1c75cdd6a1ef507a766d606
norphel <norphel317@gmail.com>
2023-04-15 16:35:40 +0530

    page structure generated by JS

--- src/index.js
+++ src/index.js
@@ -1,1 +1,39 @@
-console.log("Hello, Webpack Check...");
+import renderHomepage from './home.js';
+import renderMenupage from './menu.js';
+import renderContactpage from './contact.js';
+
+function createHeader () {
+    const header = document.createElement('header');
+
+    const home = document.createElement('div');
+    home.classList.add('home');
+
+    const logo = document.createElement('div');
+    logo.classList.add('logo');
+    const restaurantName = document.createElement('h1');
+    restaurantName.textContent = 'alchi kitchen';
+
+    home.appendChild(logo);
+    home.appendChild(restaurantName);
+
+    const nav = document.createElement('nav');
+    const menu = document.createElement('h2');
+    const contact = document.createElement('h2');
+    menu.textContent = 'Menu';
+    contact.textContent = 'Contact';
+
+    nav.appendChild(menu);
+    nav.appendChild(contact);
+
+    header.appendChild(home);
+    header.appendChild(nav);
+
+    return header;
+}
+
+(function () {
+    document.body.appendChild(createHeader());
+    const main = document.createElement('main');
+    document.body.appendChild(main);
+    renderHomepage();
+})();
