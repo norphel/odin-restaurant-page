76bd2a2f4430e84d30074a5fb54f6f46ab147055
norphel <norphel317@gmail.com>
2023-04-23 23:44:42 +0530

    update menu page

--- src/menu.js
+++ src/menu.js
@@ -3,6 +3,8 @@ import buckwheatCrepePhoto from './assets/buckwheat_crepe.png';
 import chutagiPhoto from './assets/chutagi.png';
 import khambirPhoto from './assets/khambir.png';
 import pabaPhoto from './assets/paba.png';
+import tashiTagyeTeaPhoto from './assets/tashiTagyeTea.png';
+import thukpaPhoto from './assets/thukpa.png';
 
 
 function createMenuCard(itemName, description, itemPhoto) {
@@ -35,39 +37,53 @@ function renderMenupage () {
 
     const khambir = createMenuCard(
         'Khambir',
-        'Khambir is the sourdough bread of Ladakh. The chef has given a modern twist to it in the way of providing it in stuffed form where both veg and non-veg stuffing are available',
+        'Khambir is the sourdough bread of Ladakh. Start your day with a khambir stuffed with egg-potato salad topped with peri-peri which adds some zing to the dish. Many other stuffing variations are also available in our menu.',
         khambirPhoto
     );
 
     const paba = createMenuCard(
         'Paba and Tangtur',
-        'Paba is made from roasted barley. Tangtur is made with either curd or buttermilk in which herbs like coriander, chives, etc are added.',
+        'Paba is usually made from roasted barley but multi-grain variations also exist. The flour grinded with water mill preserves the taste and nutrition. Tangtur is basically curd or buttermilk mixed with herbs like coriander, chives, etc.',
         pabaPhoto
     );
 
     const chutagi = createMenuCard(
         'Chutagi',
-        'It is the Ladakhi Pasta made with wheat in the shape of bowties. It comes in both vegetarian and non-vegetarian variants. It is a highly nutritiuos food eaten by Ladakhis throughout the year.',
+        'Also known as Ladakhi pasta, small bowtie shape pasta is cooked in a stew of various vegetables, mutton, etc. In order to preserve its nutritional value, the pasta is made with wholegrain wheat instead of refined flour.',
         chutagiPhoto
     );
 
     const buckwheatCrepe = createMenuCard(
         'Buckwheat Crepe',
-        'Buckwheat is considered a super food. Crepe made from it highly delicious when eaten with a fresh homemade apricot jam with a cup of refreshing tea',
+        'Buckwheat is considered a superfood. A sumptuous breakfast of buckwheat crepe, oats chia yogurt, red juicy apples, and walnuts is the perfect way to start your day.',
         buckwheatCrepePhoto
     );
 
     const apricotJam = createMenuCard(
         'Apricot Jam',
-        'Apricots are found throughout Ladakh. The white kernel apricot is very sweet, delicious, and healthy. The jam can be made at home with freshly picked apricots. We can eat it with any bread',
+        'Apricots are found throughout Ladakh. Get some fresh apricots and make your own jam at home with some cinnamon and mint and enjoy with any bread.',
         apricotJamPhoto
     );
+
+    const tashiTagyeTea = createMenuCard(
+        'Tashi Tagye Tea',
+        '"Tashi Tagye" means eight auspicious symbols. This tea is made with 8 aromatic and healthy spices and herbs which not only refreshes you but also keeps you healthy.',
+        tashiTagyeTeaPhoto
+    );
+
+    const thukpa = createMenuCard(
+        'Thukpa',
+        'Thukpa is a kind of soup/stew usually eaten during cold winter nights. A combination of noodles, vegetables, Ladakhi sun-dried cheese called "churpe", etc. makes it a highly tasty and healthy food.',
+        thukpaPhoto
+    )
 
     menus.appendChild(khambir);
     menus.appendChild(paba);
     menus.appendChild(chutagi);
     menus.appendChild(buckwheatCrepe);
     menus.appendChild(apricotJam);
+    menus.appendChild(tashiTagyeTea);
+    menus.appendChild(thukpa);
     
     main.appendChild(menus);
 
